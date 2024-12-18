e00051e8acb50c6495de9049e4e9d464329f9dc0
aqianer <375264725@qq.com>
2024-12-18 13:57:16 +0800

    yuoj upload

---
+++ yuoj-backend-judge-service/src/test/java/com/yupi/yuojbackendjudgeservice/YuojBackendJudgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.yuojbackendjudgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = YuojBackendJudgeServiceApplicationTests.class)
+class YuojBackendJudgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ yuoj-backend-service-client/src/test/java/com/yupi/yuojbackendserviceclient/YuojBackendServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.yuojbackendserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = YuojBackendServiceClientApplicationTests.class)
+class YuojBackendServiceClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
